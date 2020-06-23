46f8318ef4c25a4d15c964c805bf36d70f2d3e63
gteng <greg.teng@nike.com>
2020-06-23 08:20:03 +0800

    cmd

--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,18 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const {
   CleanWebpackPlugin
 } = require('clean-webpack-plugin');
+const webpack = require('webpack')
 
 
 module.exports = {
   mode: 'development',
   entry: './src/main.js',
+  devServer: {
+    port: 8000,
+    host: '0.0.0.0',
+    contentBase: './dist',
+    hot: true
+  },
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
@@ -20,12 +27,22 @@ module.exports = {
         'css-loader',
         'sass-loader'
       ]
+    }, {
+      test: /\.(png|jpg|gif)$/,
+      use: [{
+        loader: 'url-loader',
+        options: {
+          limit: 8192
+        }
+      }]
     }]
   },
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       title: 'Output Management'
-    })
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NamedModulesPlugin()
   ],
 }
